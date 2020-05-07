17f04af26247f08f4c5dff55a2a7097d99e58fdd
Xilin Chen <xilinc@med.umich.edu>
2020-05-07 10:46:29 -0400

    just skeleton

---
+++ R/computer_config.R
@@ -0,0 +1,42 @@
+# This script detects the computer enviromnent automatically 
+# and assiagn variable `Brian_George_folder_loc` to existed directory.
+# if the locations changed, you can change the value assiagned to Brian_George_folder_loc
+
+# output: Brian_George_folder_loc
+
+type = Sys.info()[['sysname']]
+
+if (type == "Darwin") {
+  Brian_George_folder_loc = "/Volumes/George_Surgeon_Projects/"
+  file_exist = dir.exists(Brian_George_folder_loc)
+  
+  if (file_exist == TRUE) {
+    message("Mac Envirment; Brian_George_folder_loc is located at: ",
+            Brian_George_folder_loc)
+  } else if (file_exist == FALSE) {
+    rm(type, file_exist)
+    stop(
+      "Mac Envirment; Brian_George_folder_loc is NOT located at:",
+      Brian_George_folder_loc,
+      ". Please Check if you are connected to Maize"
+    )
+  }
+  
+} else if (type == "Windows") {
+  Brian_George_folder_loc = "X:\\George_Surgeon_Projects/"
+  file_exist = dir.exists(Brian_George_folder_loc)
+  
+  if (file_exist == TRUE) {
+    message("Windows PC Envirment; Brian_George_folder_loc is located at:",
+            Brian_George_folder_loc)
+  } else if (file_exist == FALSE) {
+    rm(type, file_exist)
+    stop(
+      "Windows PC Envirment; Brian_George_folder_loc is NOT located at:",
+      Brian_George_folder_loc,
+      ". Please Check if your Brian_George_folder_loc is mapped differently"
+    )
+  }
+}
+
+rm(type, file_exist)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-medicareR
+
+# medicareR
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of medicareR is to ...
+
+## Installation
+
+You can install the released version of medicareR from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("medicareR")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(medicareR)
+## basic example code
+```
+
