45c8f46b26094d076291e68a66864c673dcb7679
Xilin Chen <xilinc@med.umich.edu>
2020-05-12 18:11:13 -0400

    computer config for bg loc

--- R/computer_config.R
+++ R/computer_config.R
@@ -1,16 +1,17 @@
-#' Title: find brian geroge folder location
-#' This script detects the computer enviromnent automatically
-# and assiagn variable `Brian_George_folder_loc` to existed directory.
-# if the locations changed, you can change the value assiagned to Brian_George_folder_loc
+#' Find Brian Geroge folder location
+#' @description  This script detects the computer enviromnent automatically
+#'     and assign variable `Brian_George_folder_loc` to an detected directory.
+#'    If the locations can't be detected, you manually assign  value to Brian_George_folder_loc
 #'
-#' @param your_BG_folder_loc put your own BG location if the function can't find yours. e.g."/Volumes/George_Surgeon_Projects/"
+#' @param your_BG_folder_loc put your own BG location if the function can't find yours. e.g."/Volumes/George_Surgeon_Projects"
 #'
-#' @return Brian_George folder location on maize, if it can't automaticly detect the location, you
+#' @return Brian_George folder location on maize, if it can't automatically detect the location, you
 #' can also manually specify the location
 #' @export
 #'
 #' @examples
-#'
+#' # if this can't detect your BG folder, you can assign it by
+#' find_maize_folder(your_BG_folder_loc = "/Volumes/George_Surgeon_Projects")
 #'
 #'
 find_maize_folder <- function(your_BG_folder_loc = NA) {
@@ -18,7 +19,7 @@ find_maize_folder <- function(your_BG_folder_loc = NA) {
     type = Sys.info()[['sysname']]
 
     if (type == "Darwin") {
-      Brian_George_folder_loc = "/Volumes/George_Surgeon_Projects/"
+      Brian_George_folder_loc = "/Volumes/George_Surgeon_Projects"
       file_exist = dir.exists(Brian_George_folder_loc)
 
       if (file_exist == TRUE) {
@@ -38,7 +39,7 @@ find_maize_folder <- function(your_BG_folder_loc = NA) {
       }
 
     } else if (type == "Windows") {
-      Brian_George_folder_loc = "X:\\George_Surgeon_Projects/"
+      Brian_George_folder_loc = "X:\\George_Surgeon_Projects"
       file_exist = dir.exists(Brian_George_folder_loc)
 
       if (file_exist == TRUE) {
@@ -58,6 +59,13 @@ find_maize_folder <- function(your_BG_folder_loc = NA) {
       }
     }
   } else if (!is.na(your_BG_folder_loc)) {
-    your_BG_folder_loc
+    file_exist = dir.exists(your_BG_folder_loc)
+    if (file_exist == TRUE){
+      return(your_BG_folder_loc)
+      message("Your assigned Brian_George_folder_loc at:",
+              your_BG_folder_loc)
+    } else if (file_exist == FALSE){
+      stop(your_BG_folder_loc, " doesn't exist. Check if you assigned the right location.")
+    }
   }
 }
