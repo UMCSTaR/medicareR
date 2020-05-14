0e44f5b5066099eec08df12491d1b679504d5400
Xilin Chen <xilinc@med.umich.edu>
2020-05-14 15:37:19 -0400

    make data_dir more flexible

--- R/mk_data_dir_path.R
+++ R/mk_data_dir_path.R
@@ -3,12 +3,13 @@
 #'     This was based on the mapped Maize location on Xilin Chen's Mac and Windows
 
 #' @param data_root : "sample" or "full_data"
+#'
 #' @param medicare_file : original medicare data file folder name. e.g. demon data folder
+#' @param george_file Brian_George folder loc on Maize
+#' @param medicare_data_file original medicare locations
+#' @param medicare_std_data_file  where the std medicare data will be saved to location
+
 #' name is "Denom_MBSF"
-#' @param george_file: where the BG folder located at on maize. You may use the `computer_config`
-#'    to help you detect.
-#'
-#'
 #' @return list items of data directory locactions.
 #' @export
 #'
@@ -16,12 +17,14 @@
 #' \dontrun{
 #' working_dir_config(data_root = "sample",
 #'                    medicare_file = "Denom_MBSF",
-#'                    george_file = ""/Volumes/George_Surgeon_Projects")
+#'                    george_file = /Volumes/George_Surgeon_Projects")
 #' }
 
 mk_data_dir_path <- function(data_root = "sample",
-                               medicare_file,
-                               george_file) {
+                             medicare_file = NULL,
+                             george_file,
+                             medicare_data_file = "/original_medicare_selected_vars/data",
+                             medicare_std_data_file = "/standardized_medicare_data_using_R/std") {
   # working directory config
   wd <- list()
 
@@ -35,10 +38,6 @@ mk_data_dir_path <- function(data_root = "sample",
     message("the Brian George Maize folder is located at: ", wd$george_file)
   }
 
-  # original medicare locations
-  wd$medicare_data_file <- "/original_medicare_selected_vars/data"
-  # where the std medicare data will be saved to location
-  wd$medicare_std_data_file<-"/standardized_medicare_data_using_R/std"
 
   # input data location: like neppes npi, etc.
   wd$input_data = paste0(wd$george_file,  "/standardized_medicare_data_using_R/input")
@@ -47,17 +46,17 @@ mk_data_dir_path <- function(data_root = "sample",
   # source root
   if (data_root == "sample") {
     wd$src_data_root <-
-      paste0(wd$george_file, wd$medicare_data_file, "/sample/", medicare_file, "/")
+      paste0(wd$george_file, medicare_data_file, "/sample/", medicare_file, "/")
     wd$std_data_root <-
-      paste0(wd$george_file, wd$medicare_std_data_file, "/sample/")
+      paste0(wd$george_file, medicare_std_data_file, "/sample/")
 
     wd
   }
   else if (data_root == "full_data") {
     wd$src_data_root <-
-      paste0(wd$george_file, wd$medicare_data_file, "/", medicare_file, "/")
+      paste0(wd$george_file, medicare_data_file, "/", medicare_file, "/")
     wd$std_data_root <-
-      paste0(wd$george_file, wd$medicare_std_data_file, "/full_data/")
+      paste0(wd$george_file, medicare_std_data_file, "/full_data/")
 
     wd
   }
