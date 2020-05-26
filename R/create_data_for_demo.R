73982751495aa08c4ed32301ff451705d46d0b3b
Xilin Chen <xilinc@med.umich.edu>
2020-05-26 09:56:17 -0400

    add demon mock data

--- R/create_data_for_demo.R
+++ R/create_data_for_demo.R
@@ -1,3 +1,5 @@
+# comment out to avoit it running when building the package.
+
 # prof_clm = fread("/Volumes/George_Surgeon_Projects/standardized_medicare_data_using_R/std/sample/prof_clm.csv")
 #
 # prof_clm_not_real = prof_clm %>%
@@ -18,3 +20,41 @@
 #   sample_n(50)
 #
 # usethis::use_data(prof_clm_code_not_real)
+
+
+# MBSF data ---------
+# year = c(2007:2017)
+#
+# denom_data_loc = paste0(
+#   "/Volumes/George_Surgeon_Projects/original_medicare_selected_vars/data/sample/Denom_MBSF/denom",
+#   stringr::str_sub(year, 3, 4),
+#   ".csv"
+# )
+#
+# for (i in seq_along(year)){
+# data = fread(denom_data_loc[[i]], nrows = 1000) %>%
+#   sample_n(80)
+#
+# data = data %>%
+#  mutate_at(vars(contains('bene_id')), ~stringi::stri_rand_strings(80,10)) %>%
+#  mutate_at(vars(contains('ZIP')), ~stringi::stri_rand_strings(80,10, "[0-9]")) %>%
+#  sample_n(10)
+#
+# glimpse(data)
+#
+# assign(paste0("denom", stringr::str_sub(year[[i]], 3, 4)), data)
+# }
+#
+# usethis::use_data(denom07)
+# usethis::use_data(denom08)
+# usethis::use_data(denom09)
+# usethis::use_data(denom10)
+# usethis::use_data(denom11)
+# usethis::use_data(denom12)
+# usethis::use_data(denom13)
+# usethis::use_data(denom14)
+# usethis::use_data(denom15)
+# usethis::use_data(denom16)
+# usethis::use_data(denom17)
+#
+
