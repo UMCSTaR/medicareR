91c6779864fe4d0e379a113c02914488bebb149f
Xilin Chen <xilinc@med.umich.edu>
2020-10-16 09:58:10 -0400

    ses_info,fix warning

--- R/ses_info.R
+++ R/ses_info.R
@@ -13,20 +13,26 @@ ses_info <- function(ses = ses,
   ses <- ses %>%
     transmute(
       zip_cd = str_split_fixed(GEO_id, "US", n = 2)[, 2],
-      val_mhhi = as.numeric(MHHI__HC02_EST_VC02_), # middle house hold income
+      # middle house hold income
+      val_mhhi = suppressWarnings(as.numeric(MHHI__HC02_EST_VC02_)), # MHHI__HC02_EST_VC02_ has character value, but we only need number here,
+                                                                     # so the warning is suppressed.
       e_ses_quintile = ntile(val_mhhi, 5)
     ) %>%
     mutate(zip_cd = as.integer(zip_cd))
 
-  analytic_ses <- left_join(original_dataset, ses, by = "zip_cd")
+  # left join
+  analytic_ses = merge.data.table(original_dataset, ses, by = "zip_cd", all.x = TRUE)
 
   if (!"NULL" %in% class(ses_zscore)){
-    analytic_ses <- left_join(analytic_ses,
-                            ses_zscore %>%
-                              select(zip_cd, starts_with("e_ses_")) %>%
-                              mutate(zip_cd = as.integer(zip_cd)),
-                            by = "zip_cd")
+    ses_zscore = ses_zscore %>%
+      select(zip_cd, starts_with("e_ses_")) %>%
+      mutate(zip_cd = as.integer(zip_cd))
+
+    # left join
+    analytic_ses <- merge.data.table(analytic_ses,
+                                      ses_zscore,
+                                      by = "zip_cd", all.x = TRUE)
   }
 
-  analytic_ses
+  setDT(analytic_ses)
 }
