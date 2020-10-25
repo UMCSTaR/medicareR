6887efbafacfece43161ce47b0bcdcb0a6456a15
xilinc <xilinc@med.umich.edu>
2020-10-25 10:30:13 -0400

    procedure_select add selected cols

--- R/procedure_selection.R
+++ R/procedure_selection.R
@@ -25,7 +25,7 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
 
   # check cpt_map data has 3 variables
   if (any(!c("cpt_cd", "e_proc_grp", "e_proc_grp_lbl") %in%
-    names(cpt_map))) {
+          names(cpt_map))) {
     stop("assigned cpt_map doesn't include all vars: cpt_cd, e_proc_grp, e_proc_grp_lbl")
   }
 
@@ -52,7 +52,7 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
 
   # read prof clm code data ------
   prof_clm_loc = paste0(std_data_root, prof_codes_folder, "/",
-                         list.files(paste0(std_data_root, prof_codes_folder)))
+                        list.files(paste0(std_data_root, prof_codes_folder)))
 
   # unique claim based on
   clm_distinct_vars <-
@@ -68,27 +68,38 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
   # read std pro claim data
   if(is.na(test_sas_processed_data_loc)){
 
-  prof_clm_select_list = list()
-  for (i in seq_along(prof_clm_loc)) {
-    message(
-      "reading prof_clm year ",
-      stringr::str_extract(prof_clm_loc, "[0-9]+")[i],
-      " data...."
-    )
-    prof_clm <- fread(prof_clm_loc[i], colClasses = "character")
+    prof_clm_select_list = list()
+    for (i in seq_along(prof_clm_loc)) {
+      message(
+        "reading prof_clm year ",
+        stringr::str_extract(prof_clm_loc, "[0-9]+")[i],
+        " data...."
+      )
+      # Read in selected columns--
+      col2keep = fread(prof_clm_loc[i], nrows = 0) %>%
+        as_tibble() %>%
+        select(member_id,
+               svc_start_dt,
+               svc_end_dt,
+               provider_npi,
+               cpt_cd,
+               contains("cpt_mod"),
+               provider_splty) %>% names()
 
-    # keep defined CPT code and
-    # drop professional claim that don't have NPI
-    message("filtering procedures....")
+      prof_clm <- fread(prof_clm_loc[i], select = col2keep,
+                        colClasses=list(character=col2keep))
 
-    # filter procedure and NPI
-    prof_clm_select <- prof_clm[cpt_cd %in% cpt_map$cpt_cd &
-                                  provider_npi != ""]
 
-    prof_clm_select_list[[i]] = prof_clm_select
-  }
+      # keep defined CPT code and
+      # drop professional claim that don't have NPI
+      message("filtering procedures....")
 
-  prof_clm_select = rbindlist(prof_clm_select_list, fill = T)
+      # filter procedure and NPI
+      prof_clm_select_list[[i]] = prof_clm[cpt_cd %in% cpt_map$cpt_cd &
+                                             provider_npi != ""]
+    }
+
+    prof_clm_select = rbindlist(prof_clm_select_list, fill = T)
 
   } else if (!is.na(test_sas_processed_data_loc)) {
     # for test purpose, comparing with SAS processed data
@@ -100,23 +111,16 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
   }
 
   # cpt mod wide to long format
-  prof_clm_select = prof_clm_select %>%
-    melt(
-      measure = patterns("cpt_mod"),
-      value.name = "cpt_mod"
-    )
+  prof_clm_select =
+    melt(prof_clm_select,
+         measure = patterns("cpt_mod"),
+         value.name = "cpt_mod")
 
-  # select vars
-  prof_clm_select_splty = prof_clm_select[, .(member_id,
-                                              svc_start_dt,
-                                              svc_end_dt,
-                                              provider_npi,
-                                              cpt_cd,
-                                              cpt_mod,
-                                              provider_splty)]
+  # delete variable created
+  prof_clm_select[, variable := NULL]
 
   # unique cases
-  analytic_cptmod = unique(prof_clm_select_splty, by = clm_distinct_vars)
+  analytic_cptmod = unique(prof_clm_select, by = clm_distinct_vars)
 
   # keep cpt_mod code to one cell if it is has the same group_by info
   # e.g. if two claims have the same group_by listed vars, then two claims become one with two mod code
