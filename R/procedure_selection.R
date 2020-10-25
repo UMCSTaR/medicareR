4280eeeba3c6024fac4a77f1ea043be8bf7b7598
Xilin Chen <xilinc@med.umich.edu>
2020-10-25 13:22:40 -0400

    select CPT furrr with progress bar

--- R/procedure_selection.R
+++ R/procedure_selection.R
@@ -4,6 +4,7 @@
 #' @param std_data_root     professional claim data location
 #' @param prof_codes_folder professional claim folder name
 #' @param cpt_map           should include 3 variable: cpt_cd, e_proc_grp and e_proc_grp_lbl
+#' @param n_worker          number of workers to use in future map
 #' @param test_sas_processed_data_loc this is for testing, only assign value when you want to test,
 #'     eg. compare the processing results with SAS code Zhaohui Fan created.
 #'     Otherwise leave it NA. When testing, assign the location of sas pre processed prof_clm data.
@@ -14,6 +15,8 @@
 #'  3. delete duplicated claims
 #'  4. aggregate cpt_mode (if two cases have same info but diff cpt_mode, aggregate as one case include two cpt_mode code)
 #'
+#' @import progressr
+#'
 #' @return
 #' @export
 #'
@@ -21,7 +24,8 @@
 procedure_selection <- function(std_data_root = wd$std_data_root,
                                 prof_codes_folder = "prof_clm",
                                 cpt_map = define_proc_by_cpt,
-                                test_sas_processed_data_loc = NA) {
+                                test_sas_processed_data_loc = NA,
+                                n_worker = 2) {
 
   # check cpt_map data has 3 variables
   if (any(!c("cpt_cd", "e_proc_grp", "e_proc_grp_lbl") %in%
@@ -50,6 +54,37 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
     ))
   }
 
+  # filter CPT function---
+  filter_cpt <- function(prof_clm_loc) {
+
+    # progress info display (progressr)--
+    year = stringr::str_extract(prof_clm_loc, "[0-9]+")
+    p(sprintf("year=%s", year))
+
+
+    message("read prof_clm year ", year, " data....")
+    # Read in selected columns--
+    col2keep = fread(prof_clm_loc, nrows = 0) %>%
+      as_tibble() %>%
+      select(
+        member_id,
+        svc_start_dt,
+        svc_end_dt,
+        provider_npi,
+        cpt_cd,
+        contains("cpt_mod"),
+        provider_splty
+      ) %>% names()
+
+    prof_clm <- fread(prof_clm_loc,
+                      select = col2keep,
+                      colClasses = list(character = col2keep))
+
+    # keep defined CPT code and
+    # drop professional claim that don't have NPI
+    prof_clm[cpt_cd %in% cpt_map$cpt_cd & provider_npi != ""]
+  }
+
   # read prof clm code data ------
   prof_clm_loc = paste0(std_data_root, prof_codes_folder, "/",
                         list.files(paste0(std_data_root, prof_codes_folder)))
@@ -68,38 +103,14 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
   # read std pro claim data
   if(is.na(test_sas_processed_data_loc)){
 
-    prof_clm_select_list = list()
-    for (i in seq_along(prof_clm_loc)) {
-      message(
-        "reading prof_clm year ",
-        stringr::str_extract(prof_clm_loc, "[0-9]+")[i],
-        " data...."
-      )
-      # Read in selected columns--
-      col2keep = fread(prof_clm_loc[i], nrows = 0) %>%
-        as_tibble() %>%
-        select(member_id,
-               svc_start_dt,
-               svc_end_dt,
-               provider_npi,
-               cpt_cd,
-               contains("cpt_mod"),
-               provider_splty) %>% names()
+    message("read in prof_clm_code data.....this takes awhile")
+    plan(multisession, workers = n_worker)
 
-      prof_clm <- fread(prof_clm_loc[i], select = col2keep,
-                        colClasses=list(character=col2keep))
-
-
-      # keep defined CPT code and
-      # drop professional claim that don't have NPI
-      message("filtering procedures....")
-
-      # filter procedure and NPI
-      prof_clm_select_list[[i]] = prof_clm[cpt_cd %in% cpt_map$cpt_cd &
-                                             provider_npi != ""]
-    }
-
-    prof_clm_select = rbindlist(prof_clm_select_list, fill = T)
+    # show progress bar
+    with_progress({
+      p <- progressor(steps = length(prof_clm_loc))
+      prof_clm_select = future_map_dfr(prof_clm_loc, filter_cpt)
+    })
 
   } else if (!is.na(test_sas_processed_data_loc)) {
     # for test purpose, comparing with SAS processed data
@@ -110,6 +121,7 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
                                   provider_npi != ""]
   }
 
+  message("finsihed reading data; processing data now.....")
   # cpt mod wide to long format
   prof_clm_select =
     melt(prof_clm_select,
