3f088a229c50775f13e34187527646173e512623
Xilin Chen <xilinc@med.umich.edu>
2021-04-22 16:47:16 -0400

    procedure selection by year
    multiple years processing always cause memory problems, and it's not scalable. So we change to year by year process

--- R/procedure_selection.R
+++ R/procedure_selection.R
@@ -4,6 +4,7 @@
 #' @param std_data_root     professional claim data location
 #' @param prof_codes_folder professional claim folder name
 #' @param cpt_map           should include 3 variable: cpt_cd, e_proc_grp and e_proc_grp_lbl
+#' @param year              select one year to process, e.g. 2007
 #' @param n_worker          number of workers to use in future map
 #' @param test_sas_processed_data_loc this is for testing, only assign value when you want to test,
 #'     eg. compare the processing results with SAS code Zhaohui Fan created.
@@ -24,9 +25,11 @@
 procedure_selection <- function(std_data_root = wd$std_data_root,
                                 prof_codes_folder = "prof_clm",
                                 cpt_map = define_proc_by_cpt,
+                                year = 2007,
                                 test_sas_processed_data_loc = NA,
                                 n_worker = 2) {
 
+  # Set uclm_distinct_varsp and checks -------------------------------------------------------
   # check cpt_map data has 3 variables
   if (any(!c("cpt_cd", "e_proc_grp", "e_proc_grp_lbl") %in%
           names(cpt_map))) {
@@ -54,15 +57,32 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
     ))
   }
 
-  # filter CPT function---
+  # data processing ---------------------------------------------------------
+
+  # read prof clm code data ------
+  if(year == "all") {
+    prof_clm_loc = paste0(std_data_root, prof_codes_folder, "/",
+                          list.files(paste0(std_data_root, prof_codes_folder)))
+  } else {
+    prof_clm_loc = paste0(std_data_root, prof_codes_folder, "/prof_clm_", year, ".csv")
+  }
+
+  # check if files locations existed
+  # if any of the file location doesn't exist, error will occur
+  if(!all(file.exists(prof_clm_loc))){
+    stop(paste("not all files existed. please check if file locations are correct at",
+               prof_clm_loc))
+  }
+
+  # filter CPT based on provided map
   filter_cpt <- function(prof_clm_loc) {
 
     # progress info display (progressr)--
-    year = stringr::str_extract(prof_clm_loc, "[0-9]+")
-    p(sprintf("year=%s", year))
+    process_year = stringr::str_extract(prof_clm_loc, "[0-9]+")
+    p(sprintf("year=%s", process_year))
 
 
-    message("read prof_clm year ", year, " data....")
+    message("read prof_clm year ", process_year, " data....")
     # Read in selected columns--
     col2keep = fread(prof_clm_loc, nrows = 0) %>%
       as_tibble() %>%
@@ -85,35 +105,22 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
     prof_clm[cpt_cd %in% cpt_map$cpt_cd & provider_npi != ""]
   }
 
-  # read prof clm code data ------
-  prof_clm_loc = paste0(std_data_root, prof_codes_folder, "/",
-                        list.files(paste0(std_data_root, prof_codes_folder)))
 
-  # unique claim based on
-  clm_distinct_vars <-
-    c(
-      "member_id",
-      "svc_start_dt",
-      "svc_end_dt",
-      "provider_npi",
-      "cpt_cd",
-      "cpt_mod"
-    )
-
-  # read std pro claim data
   if(is.na(test_sas_processed_data_loc)){
-
+    # read csv data-----------------------
     message("read in prof_clm_code data.....this takes awhile")
     plan(multisession, workers = n_worker)
 
     # show progress bar
     with_progress({
       p <- progressor(steps = length(prof_clm_loc))
+      # use filter_cpt function
       prof_clm_select = future_map_dfr(prof_clm_loc, filter_cpt)
     })
 
   } else if (!is.na(test_sas_processed_data_loc)) {
-    # for test purpose, comparing with SAS processed data
+    # for test purpose, comparing with SAS processed data -----
+    # read std pro claim data ---
     prof_clm = haven::read_sas(paste0(test_sas_processed_data_loc))
 
     data.table::setDT(prof_clm)
@@ -121,7 +128,7 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
                                   provider_npi != ""]
   }
 
-  message("finsihed reading data; processing data now.....")
+  message("finished reading data; processing data now.....")
   # cpt mod wide to long format
   prof_clm_select =
     melt(prof_clm_select,
@@ -132,6 +139,16 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
   prof_clm_select[, variable := NULL]
 
   # unique cases
+  # unique claim based on
+  clm_distinct_vars <-
+    c(
+      "member_id",
+      "svc_start_dt",
+      "svc_end_dt",
+      "provider_npi",
+      "cpt_cd",
+      "cpt_mod"
+    )
   analytic_cptmod = unique(prof_clm_select, by = clm_distinct_vars)
 
   # keep cpt_mod code to one cell if it is has the same group_by info
@@ -146,12 +163,13 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
       cpt_cd
     ) %>%
     mutate(mod_n = row_number()) %>%
-    as.data.table() %>%
+    as.data.frame() %>%
     tidyr::pivot_wider(
       names_from = mod_n,
       values_from = cpt_mod,
       names_prefix = "mod"
-    ) %>%
+    )%>%
+    as.data.frame() %>%
     tidyr::unite(
       "cpt_mod",
       starts_with("mod"),
@@ -171,5 +189,5 @@ procedure_selection <- function(std_data_root = wd$std_data_root,
       dt_profsvc_end = svc_end_dt,
       id_physician_npi = provider_npi
     ) %>%
-    as.data.table()
+    as.data.frame()
 }
