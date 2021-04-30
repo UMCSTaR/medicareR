bc02f8f94d0bca931926924f2e99081e1703a913
Xilin Chen <xilinc@med.umich.edu>
2021-04-30 11:50:12 -0400

    ses_info fix zip code

--- R/ses_info.R
+++ R/ses_info.R
@@ -7,18 +7,19 @@
 #'
 #' @export
 
-ses_info <- function(ses = ses,
-                     ses_zscore = NULL,
-                     original_dataset = analytic_elix) {
+ses_info <- function(original_dataset = analytic_elix,
+                     ses = ses,
+                     ses_zscore = NULL
+                     ) {
   ses <- ses %>%
     transmute(
       zip_cd = str_split_fixed(GEO_id, "US", n = 2)[, 2],
       # middle house hold income
       val_mhhi = suppressWarnings(as.numeric(MHHI__HC02_EST_VC02_)), # MHHI__HC02_EST_VC02_ has character value, but we only need number here,
-                                                                     # so the warning is suppressed.
+      # so the warning is suppressed.
       e_ses_quintile = ntile(val_mhhi, 5)
     ) %>%
-    mutate(zip_cd = as.integer(zip_cd))
+    mutate(zip_cd = as.character(zip_cd))
 
   # left join
   analytic_ses = merge.data.table(original_dataset, ses, by = "zip_cd", all.x = TRUE)
@@ -26,12 +27,12 @@ ses_info <- function(ses = ses,
   if (!"NULL" %in% class(ses_zscore)){
     ses_zscore = ses_zscore %>%
       select(zip_cd, starts_with("e_ses_")) %>%
-      mutate(zip_cd = as.integer(zip_cd))
+      mutate(zip_cd = as.character(zip_cd))
 
     # left join
     analytic_ses <- merge.data.table(analytic_ses,
-                                      ses_zscore,
-                                      by = "zip_cd", all.x = TRUE)
+                                     ses_zscore,
+                                     by = "zip_cd", all.x = TRUE)
   }
 
   setDT(analytic_ses)
