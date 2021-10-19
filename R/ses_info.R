cfd8bab6aa7b75c2de50fae00a0abf2245c3e63c
Xilin Chen <xilinc@med.umich.edu>
2021-10-19 14:14:27 -0400

    ses info fix leading 0 bug - by TG

--- R/ses_info.R
+++ R/ses_info.R
@@ -6,25 +6,28 @@
 #' @param original_dataset data to add on
 #'
 #' @export
-
 ses_info <- function(original_dataset = analytic_elix,
                      ses = ses,
-                     ses_zscore = NULL
-                     ) {
+                     ses_zscore = NULL) {
   ses <- ses %>%
     transmute(
       zip_cd = str_split_fixed(GEO_id, "US", n = 2)[, 2],
       # middle house hold income
-      val_mhhi = suppressWarnings(as.numeric(MHHI__HC02_EST_VC02_)), # MHHI__HC02_EST_VC02_ has character value, but we only need number here,
+      val_mhhi = suppressWarnings(as.numeric(MHHI__HC02_EST_VC02_)),
+      # MHHI__HC02_EST_VC02_ has character value, but we only need number here,
       # so the warning is suppressed.
       e_ses_quintile = ntile(val_mhhi, 5),
       zip_cd = as.character(zip_cd)
     )
 
+  original_dataset = original_dataset %>% mutate(zip_cd = as.character(zip_cd),
+                                                 zip_cd = str_pad(zip_cd, 5, side =
+                                                                    "left", pad = "0"))
+
   # left join
   analytic_ses = merge.data.table(original_dataset, ses, by = "zip_cd", all.x = TRUE)
 
-  if (!"NULL" %in% class(ses_zscore)){
+  if (!"NULL" %in% class(ses_zscore)) {
     ses_zscore = ses_zscore %>%
       select(zip_cd, starts_with("e_ses_")) %>%
       mutate(zip_cd = as.character(zip_cd))
